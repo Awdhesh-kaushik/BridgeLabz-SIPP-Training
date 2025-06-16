8420197ad8a845b1b1ed7886a1db2643848ce20c
Awdhesh kaushik <122629461+Awdhesh-kaushik@users.noreply.github.com>
2025-06-16 09:01:04 +0530

    Added : To check employee should get bonus or not

---
+++ Java Control Flows/employe bonus.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bonus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your salary: ");
+        double salary = sc.nextDouble();
+        System.out.print("Enter your years of service: ");
+        int years = sc.nextInt();
+
+        if (years > 5) {
+            double bonus = 0.05 * salary;
+            System.out.println("Bonus amount: " + bonus);
+        } else {
+            System.out.println("No bonus");
+        }
+    }
+}
