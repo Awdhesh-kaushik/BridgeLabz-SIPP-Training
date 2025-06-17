3407ede0b900579c1b95e64c4fc601863b7ac159
Awdhesh kaushik <122629461+Awdhesh-kaushik@users.noreply.github.com>
2025-06-17 08:32:02 +0530

    Delete Day1_Programming elements directory

--- Day1_Programming elements/Average.java
+++
@@ -1,9 +0,0 @@
-public class Average{
-public static void main(String[] args){
-
-int math =94, physics =95,chemistry=96;
-int sum =math +physics+chemistry;
-double average = sum/3.0;
-
-System.out.println("Sam's average percent is:"+average+"%");
-}}
